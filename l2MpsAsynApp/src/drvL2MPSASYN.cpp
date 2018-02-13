74cffc42ca6ffa65be89826a7d17873e48ed91e0
Jesus Vasquez <jvasquez@slac.stanford.edu>
2018-02-13 12:31:47 -0800

    Use new BLM method to start the polling thread

--- l2MpsAsynApp/src/drvL2MPSASYN.cpp
+++ l2MpsAsynApp/src/drvL2MPSASYN.cpp
@@ -364,7 +364,7 @@ L2MPS::L2MPS(const char *portName, const uint16_t appId, const std::string recor
                 }
                 else if ((!appType_.compare("BLM")) | (!appType_.compare("MPS_6CH")) | (!appType_.compare("MPS_24CH")))
                 {
-                    amc[i] = MpsBlmFactory::create(mpsRoot, i, &setBlmCB);
+                    amc[i] = MpsBlmFactory::create(mpsRoot, i);
                     InitBlmMaps(i);
                 }
             }
@@ -557,6 +557,10 @@ void L2MPS::InitBlmMaps(const int bay)
     blenDbParams << ",PORT=" << std::string(portName_);
     blenDbParams << ",BAY=" << bay;
     dbLoadRecords("db/blm.db", blenDbParams.str().c_str());
+
+    // Start polling thread
+    boost::any_cast<MpsBlm>(amc[bay])->startPollThread(1, &setBlmCB);
+
 }
 
 asynStatus L2MPS::readInt32(asynUser *pasynUser, epicsInt32 *value)
