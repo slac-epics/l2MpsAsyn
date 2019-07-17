3329431317d68779ee762ca555153cf8d14e8326
Jesus Vasquez <jesusvasquez333@gmail.com>
2019-07-17 16:13:03 -0700

    Remove trailing spaces and setting tabs to 4 spaces

--- l2MpsAsynApp/src/initHooks.c
+++ l2MpsAsynApp/src/initHooks.c
@@ -1,4 +1,4 @@
-/* initHooks.c	ioc initialization hooks */ 
+/* initHooks.c  ioc initialization hooks */
 
 #include <stdio.h>
 #include <initHooks.h>
@@ -17,105 +17,117 @@
  */
 
 static void printErrorMessage(char *extraInfo) {
-  printf("ERROR: Failed to restore application MPS thresholds.\n");
-  printf("       Thresholds must be restored in order to enable MPS!\n");
-  if (strlen(extraInfo) > 0) {
-    printf("%s\n", extraInfo);
-  }
+    printf("ERROR: Failed to restore application MPS thresholds.\n");
+    printf("       Thresholds must be restored in order to enable MPS!\n");
+    if (strlen(extraInfo) > 0) {
+        printf("%s\n", extraInfo);
+    }
 }
 
 /* If this function (initHooks) is loaded, iocInit calls this function
  * at certain defined points during IOC initialization */
 static void l2MpsAsynInitHooks(initHookState state)
 {
-  char errorMessage[300];
-  char *str;
-  char *host;
-  char *portStr;
-  int len;
-   switch (state) {
-   case initHookAtBeginning :
-     break;
-   case initHookAfterCallbackInit :
-     break;
-   case initHookAfterCaLinkInit :
-     break;
-   case initHookAfterInitDrvSup :
-     break;
-   case initHookAfterInitRecSup :
-     break;
-   case initHookAfterInitDevSup :
-     break;
-   case initHookAfterInitDatabase :
-     break;
-   case initHookAfterFinishDevSup :
-     break;
-   case initHookAfterScanInit :
-     break;
-   case initHookAfterInitialProcess :
-     break;
-   case initHookAfterInterruptAccept :
-     break;
-   case initHookAtEnd :
-     host = getenv("MPS_MANAGER_HOST");
-     portStr = getenv("MPS_MANAGER_PORT");
+    char errorMessage[300];
+    char *str;
+    char *host;
+    char *portStr;
+    int len;
 
-     str = getenv("MPS_ANA_APP_ID");
-     if (str == NULL) {
-       sprintf(errorMessage, "ERROR: MPS_ANA_APP_ID environment variable not defined.\n");
-       printErrorMessage(errorMessage);
-       return;
-     }
+    switch (state)
+    {
+        case initHookAtBeginning :
+            break;
+        case initHookAfterCallbackInit :
+            break;
+        case initHookAfterCaLinkInit :
+            break;
+        case initHookAfterInitDrvSup :
+            break;
+        case initHookAfterInitRecSup :
+            break;
+        case initHookAfterInitDevSup :
+            break;
+        case initHookAfterInitDatabase :
+            break;
+        case initHookAfterFinishDevSup :
+            break;
+        case initHookAfterScanInit :
+            break;
+        case initHookAfterInitialProcess :
+            break;
+        case initHookAfterInterruptAccept :
+            break;
 
-     len = strlen(str);
+        case initHookAtEnd :
+            host = getenv("MPS_MANAGER_HOST");
+            portStr = getenv("MPS_MANAGER_PORT");
 
-     if (host == NULL) {
-       printf("WARNING: MPS_MANAGER_HOST environment variable not defined.\n");
-       printf("         Using lcls-daemon2 as host. This variable is defined by the MPS configuration.\n");
-       host = "lcls-daemon2";
-     }
+            str = getenv("MPS_ANA_APP_ID");
+            if (str == NULL)
+                {
+                sprintf(errorMessage, "ERROR: MPS_ANA_APP_ID environment variable not defined.\n");
+                printErrorMessage(errorMessage);
+                return;
+            }
 
-     if (portStr == NULL) {
-       printf("WARNING: MPS_MANAGER_PORT environment variable not defined.\n");
-       printf("         Using 1975 as port. This variable is defined by the MPS configuration.\n");
-       portStr = "1975";
-     }
+            len = strlen(str);
 
-     // ApplicationId string from environment variable should not have more that 4 chars or 
-     // have none
-     if (len == 0 || len > 4) {
-       sprintf(errorMessage, "ERROR: Invalid application ID string (%s).\n", str); 
-       printErrorMessage(errorMessage);
-       return;
-     }
+            if (host == NULL)
+            {
+                printf("WARNING: MPS_MANAGER_HOST environment variable not defined.\n");
+                printf("         Using lcls-daemon2 as host. This variable is defined by the MPS configuration.\n");
+                host = "lcls-daemon2";
+            }
 
-     char *endPtr;
-     strtol(str, &endPtr, 0);
-     if (endPtr == str) {
-       sprintf(errorMessage,
-	       "ERROR: Invalid application ID. Cannot convert string \"%s\" to a number.\n", str);
-       printErrorMessage(errorMessage);
-       return;
-     }
-     int appId = atoi(str);
-     
-     strtol(portStr, &endPtr, 0);
-     if (endPtr == portStr) {
-       sprintf(errorMessage,
-	       "ERROR: Invalid server port. Cannot convert string \"%s\" to a number.\n", portStr);
-       printErrorMessage(errorMessage);
-       return;
-     }
-     int port = atoi(portStr);
+            if (portStr == NULL)
+            {
+                printf("WARNING: MPS_MANAGER_PORT environment variable not defined.\n");
+                printf("         Using 1975 as port. This variable is defined by the MPS configuration.\n");
+                portStr = "1975";
+            }
 
-     if (restoreThresholds(host, port, appId) != 0) {
-       printErrorMessage("");
-     }
-     break;
-   default:
-     break;
-   }
-   return;
+            // ApplicationId string from environment variable should not have more that 4 chars or
+            // have none
+            if (len == 0 || len > 4)
+            {
+                sprintf(errorMessage, "ERROR: Invalid application ID string (%s).\n", str);
+                printErrorMessage(errorMessage);
+                return;
+            }
+
+            char *endPtr;
+            strtol(str, &endPtr, 0);
+            if (endPtr == str)
+            {
+                sprintf(errorMessage,
+                "ERROR: Invalid application ID. Cannot convert string \"%s\" to a number.\n", str);
+                printErrorMessage(errorMessage);
+                return;
+            }
+            int appId = atoi(str);
+
+            strtol(portStr, &endPtr, 0);
+            if (endPtr == portStr)
+            {
+                sprintf(errorMessage,
+                "ERROR: Invalid server port. Cannot convert string \"%s\" to a number.\n", portStr);
+                printErrorMessage(errorMessage);
+                return;
+            }
+            int port = atoi(portStr);
+
+            if (restoreThresholds(host, port, appId) != 0)
+            {
+                printErrorMessage("");
+            }
+
+            break;
+
+        default:
+            break;
+    }
+    return;
 }
 
 void l2MpsAsynInitHooksRegister(void)
--- l2MpsAsynApp/src/thresholds.c
+++ l2MpsAsynApp/src/thresholds.c
@@ -8,76 +8,84 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
-typedef struct {
-  int type;
-  int appId;
-  char deviceName[50];
+typedef struct
+{
+    int type;
+    int appId;
+    char deviceName[50];
 } restoreRequest;
 
-typedef struct {
-  int status; /* Status=10 is OK */
-  int appId;
-  char statusMessage[200];
+typedef struct
+{
+    int status; /* Status=10 is OK */
+    int appId;
+    char statusMessage[200];
 } restoreResponse;
 
-int restoreThresholds(char *server, int port, int appId) {
-  int sock;
-  struct sockaddr_in serverAddr;
-  socklen_t addrSize;
-  struct hostent *host;
-  char *errorMessage;
+int restoreThresholds(char *server, int port, int appId)
+{
+    int sock;
+    struct sockaddr_in serverAddr;
+    socklen_t addrSize;
+    struct hostent *host;
+    char *errorMessage;
 
-  host = gethostbyname(server);
-  if (host == NULL) {
-    printf("ERROR: Invalid host name (%s).\n", server);
-    return 1;
-  }
-  
-  sock = socket(PF_INET, SOCK_STREAM, 0);
+    host = gethostbyname(server);
+    if (host == NULL)
+    {
+        printf("ERROR: Invalid host name (%s).\n", server);
+        return 1;
+    }
 
-  serverAddr.sin_family = AF_INET;
-  serverAddr.sin_port = htons(port);
-  memcpy(&serverAddr.sin_addr, host->h_addr_list[0], host->h_length);
-  memset(serverAddr.sin_zero, '\0', sizeof(serverAddr.sin_zero));
+    sock = socket(PF_INET, SOCK_STREAM, 0);
 
-  printf("MPS: Connecting to MpsManager server at %s:%d\n", server, port);
-  addrSize = sizeof(serverAddr);
-  if (connect(sock, (struct sockaddr *) &serverAddr, addrSize) != 0) {
-    printf("ERROR: Failed to connect to server at %s:%d (errno=%d)\n",
-	   server, port, errno);
-    perror(errorMessage);
-    printf("ERROR: %s\n", errorMessage);
-    return 1;
-  }
-  
-  /* Send request */
-  restoreRequest request;
-  request.type = 3; /* Restore type */
-  request.appId = appId;
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(port);
+    memcpy(&serverAddr.sin_addr, host->h_addr_list[0], host->h_length);
+    memset(serverAddr.sin_zero, '\0', sizeof(serverAddr.sin_zero));
 
-  printf("MPS: Requesting thresholds for app=%d\n", appId);
-  if (send(sock, (char *) &request, sizeof(request), 0) != sizeof(request)) {
-    printf("ERROR: Failed to send threshold restore request to server at %s:%d\n",
-	   server, port);
-    return 1;
-  }
+    printf("MPS: Connecting to MpsManager server at %s:%d\n", server, port);
+    addrSize = sizeof(serverAddr);
+    if (connect(sock, (struct sockaddr *) &serverAddr, addrSize) != 0)
+    {
+        printf("ERROR: Failed to connect to server at %s:%d (errno=%d)\n",
+        server, port, errno);
+        perror(errorMessage);
+        printf("ERROR: %s\n", errorMessage);
+        return 1;
+    }
 
-  /* Wait for respose */
-  printf("MPS: Waiting for response...\n");
-  restoreResponse response;
-  if (read(sock, (char *) &response, sizeof(response)) != sizeof(response)) {
-    printf("ERROR: Failed to received threshold restore response from server at %s:%d\n",
-	   server, port);
-    return 1;
-  }
+    /* Send request */
+    restoreRequest request;
+    request.type = 3; /* Restore type */
+    request.appId = appId;
 
-  if (response.status != 10) {
-    printf("ERROR: Received status %d from MpsManager server, error message:\n", response.status);
-    printf("%s\n", response.statusMessage);
-    return 1;
-  }
-  printf("MPS: Thresholds restored.\n");
+    printf("MPS: Requesting thresholds for app=%d\n", appId);
+    if (send(sock, (char *) &request, sizeof(request), 0) != sizeof(request))
+    {
+        printf("ERROR: Failed to send threshold restore request to server at %s:%d\n",
+        server, port);
+        return 1;
+    }
 
-  return 0;
+    /* Wait for respose */
+    printf("MPS: Waiting for response...\n");
+    restoreResponse response;
+    if (read(sock, (char *) &response, sizeof(response)) != sizeof(response))
+    {
+        printf("ERROR: Failed to received threshold restore response from server at %s:%d\n",
+        server, port);
+        return 1;
+    }
+
+    if (response.status != 10)
+    {
+        printf("ERROR: Received status %d from MpsManager server, error message:\n", response.status);
+        printf("%s\n", response.statusMessage);
+        return 1;
+    }
+    printf("MPS: Thresholds restored.\n");
+
+    return 0;
 }
 
